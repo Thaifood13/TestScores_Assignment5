d2086b347ced753baad63852799064a787213885
Thai'Asia White <167017013+Thaifood13@users.noreply.github.com>
2025-03-24 18:38:48 -0400

    Add project files.

---
+++ FormTestScores.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace TestScores_Assignment5
+{
+    partial class FormTestScores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listTestScores = new System.Windows.Forms.ListView();
+            this.listScoreAverage = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcAverage = new System.Windows.Forms.Button();
+            this.btnSendtoFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listTestScores
+            // 
+            this.listTestScores.HideSelection = false;
+            this.listTestScores.Location = new System.Drawing.Point(36, 76);
+            this.listTestScores.Name = "listTestScores";
+            this.listTestScores.Size = new System.Drawing.Size(338, 451);
+            this.listTestScores.TabIndex = 0;
+            this.listTestScores.UseCompatibleStateImageBehavior = false;
+            this.listTestScores.View = System.Windows.Forms.View.List;
+            // 
+            // listScoreAverage
+            // 
+            this.listScoreAverage.HideSelection = false;
+            this.listScoreAverage.Location = new System.Drawing.Point(629, 76);
+            this.listScoreAverage.Name = "listScoreAverage";
+            this.listScoreAverage.Size = new System.Drawing.Size(338, 451);
+            this.listScoreAverage.TabIndex = 1;
+            this.listScoreAverage.UseCompatibleStateImageBehavior = false;
+            this.listScoreAverage.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Students\' Test Scores";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(631, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(336, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Students\' Score Averages";
+            // 
+            // btnCalcAverage
+            // 
+            this.btnCalcAverage.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcAverage.Location = new System.Drawing.Point(416, 231);
+            this.btnCalcAverage.Name = "btnCalcAverage";
+            this.btnCalcAverage.Size = new System.Drawing.Size(172, 44);
+            this.btnCalcAverage.TabIndex = 4;
+            this.btnCalcAverage.Text = "Calculate Average";
+            this.btnCalcAverage.UseVisualStyleBackColor = true;
+            this.btnCalcAverage.Click += new System.EventHandler(this.btnCalcAverage_Click);
+            // 
+            // btnSendtoFile
+            // 
+            this.btnSendtoFile.Font = new System.Drawing.Font("Modern No. 20", 10F);
+            this.btnSendtoFile.Location = new System.Drawing.Point(416, 293);
+            this.btnSendtoFile.Name = "btnSendtoFile";
+            this.btnSendtoFile.Size = new System.Drawing.Size(172, 44);
+            this.btnSendtoFile.TabIndex = 5;
+            this.btnSendtoFile.Text = "Store Average";
+            this.btnSendtoFile.UseVisualStyleBackColor = true;
+            this.btnSendtoFile.Click += new System.EventHandler(this.btnSendtoFile_Click);
+            // 
+            // FormTestScores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 566);
+            this.Controls.Add(this.btnSendtoFile);
+            this.Controls.Add(this.btnCalcAverage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listScoreAverage);
+            this.Controls.Add(this.listTestScores);
+            this.Name = "FormTestScores";
+            this.Text = "Test Score Average Calculator";
+            this.Load += new System.EventHandler(this.ListBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listTestScores;
+        private System.Windows.Forms.ListView listScoreAverage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcAverage;
+        private System.Windows.Forms.Button btnSendtoFile;
+    }
+}
+
---
+++ FormTestScores.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TestScores_Assignment5
+{
+    public partial class FormTestScores : Form
+    {
+        public FormTestScores()
+        {
+            InitializeComponent();
+        }
+
+        private StreamWriter outAveragesFile;
+
+        private void ListBox_Load(object sender, EventArgs e)
+        {
+
+            string inScores;
+            if (File.Exists("Student_TestScores.txt"))
+            {
+                try
+                {
+                    using (StreamReader inScoresFile = new StreamReader("Student_TestScores.txt"))
+                    {
+                        while ((inScores = inScoresFile.ReadLine()) != null)
+                        {
+                            this.listTestScores.Items.Add(inScores);
+                        }
+
+                    }
+                }
+
+                catch (FileNotFoundException exc)
+                {
+                    MessageBox.Show(exc.Message, "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message, "Issue with file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+
+            try
+            {
+                outAveragesFile = new StreamWriter("Student_TestAverages");
+            }
+
+            catch (DirectoryNotFoundException exc)
+            {
+                MessageBox.Show(exc.Message, "Invalid Directory", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            catch (System.IO.IOException exc)
+            {
+                MessageBox.Show(exc.Message, "Issues with File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnCalcAverage_Click(object sender, EventArgs e)
+        {
+            if (listTestScores.SelectedItems.Count > 0)
+            {
+                string selectedText = listTestScores.SelectedItems[0].Text; // Gets Selected items text
+                List<int> testScores = new List<int>();
+
+                // Extracts all numbers from selected inbox
+                foreach (Match match in Regex.Matches(selectedText, @"\d+"))
+                {
+                    if (int.TryParse(match.Value, out int testScore))
+                    {
+                        testScores.Add(testScore);
+                    }
+                }
+
+                if (testScores.Count > 0)
+                {
+                    int average = testScores.Sum() / testScores.Count; //calculates the average of the textscores
+                    if (listTestScores.SelectedItems[0] != null)
+                    {
+                        string Name = Regex.Replace(listTestScores.SelectedItems[0].Text, @"[^a-zA-Z\s]", "");
+                        listScoreAverage.Items.Add(string.Join(" ", Name, average));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Selected item contains no vaild scores", "Invaild Item Selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Please select a student's scores", "Item not selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnSendtoFile_Click(object sender, EventArgs e)
+        {
+            if (listScoreAverage.SelectedItems.Count > 0)
+            {
+                string selectedAverage = listScoreAverage.SelectedItems[0].Text;
+                try
+                {
+                    using (StreamWriter outAveragesFile = new StreamWriter("Student_TestAverages.txt", true))
+                    {
+                        outAveragesFile.WriteLine(selectedAverage);
+                    }
+                    MessageBox.Show("Average successfully written to file.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (IOException exc)
+                {
+                    MessageBox.Show(exc.Message, "Issues with File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an average to send to the file.", "Item not selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
